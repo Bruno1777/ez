b94c97dc01c6f576171998f0935889c8ad21efad
Bruno1777 <41631768+Bruno1777@users.noreply.github.com>
2018-07-26 15:13:42 +0100

    Add files via upload

---
+++ java/js/main.js
@@ -0,0 +1,32 @@
+function affb(){
+
+
+
+document.getElementById("box").style.fontWeight="900";
+
+
+}
+function italic(){
+
+
+document.getElementById("box").style.fontFamily='italic';
+
+}
+function under(){
+
+
+document.getElementById("box").style.textDecoration='underline';
+
+} 
+
+function Size(){
+
+document.getElementById("box").style.fontSize=document.getElementById("select").value;
+
+}
+function font(){
+
+
+document.getElementById("box").style.fontFamily=document.getElementById('list').value;
+
+}
